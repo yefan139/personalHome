85ef3469901a299741f91b0adf4c670dedf46412
gongsixiang <1250608765@qq.com>
2023-11-23 14:22:06 +0800

    fix: 增加https域名服务地址

--- public/config.js
+++ public/config.js
@@ -1,2 +1,3 @@
-window.assetsUrl = `http://124.222.19.53/imgs`; // 远程服务地址，开发时使用
+window.assetsUrl = `https://www.puretea.asia/imgs`; // 远程服务域名地址
+// window.assetsUrl = `http://124.222.19.53/imgs`; // 远程服务ip地址，开发时使用
 // window.assetsUrl = `${window.location.host}/imgs`;
